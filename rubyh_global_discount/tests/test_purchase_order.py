609c4feb5eab1c0748da2bf04eeba73d5292737f
Reza Briyan Pratama <reza95line@gmail.com>
2019-09-12 14:45:12 +0700

    Fix _prepare_order_with_percent_disc

--- rubyh_global_discount/tests/test_purchase_order.py
+++ rubyh_global_discount/tests/test_purchase_order.py
@@ -7,10 +7,6 @@ class TestPurchaseOrder(TransactionCase):
     def setUp(self, *args, **kwargs):
         result = super(TestPurchaseOrder, self).setUp(*args, **kwargs)
 
-        self.PurchaseOrder = self.env["purchase.order"]
-        self.PurchaseOrderLine = self.env["purchase.order.line"]
-        self.AccountInvoice = self.env["account.invoice"]
-        self.AccountInvoiceLine = self.env["account.invoice.line"]
         self.partner_id = self.env.ref("base.res_partner_1")
         self.product_id_1 = self.env.ref("product.product_product_8")
         self.product_id_2 = self.env.ref("product.product_product_11")
@@ -18,41 +14,6 @@ class TestPurchaseOrder(TransactionCase):
         (self.product_id_1 | self.product_id_2).write(
             {"purchase_method": "purchase"}
         )
-        self.base_vals = {
-            "partner_id": self.partner_id.id,
-            "order_line": [
-                (
-                    0,
-                    0,
-                    {
-                        "name": self.product_id_1.name,
-                        "product_id": self.product_id_1.id,
-                        "product_qty": 5.0,
-                        "product_uom": self.product_id_1.uom_po_id.id,
-                        "price_unit": 500.0,
-                        "date_planned": datetime.today().strftime(
-                            DEFAULT_SERVER_DATETIME_FORMAT
-                        ),
-                    },
-                ),
-                (
-                    0,
-                    0,
-                    {
-                        "name": self.product_id_2.name,
-                        "product_id": self.product_id_2.id,
-                        "product_qty": 5.0,
-                        "product_uom": self.product_id_2.uom_po_id.id,
-                        "price_unit": 250.0,
-                        "date_planned": datetime.today().strftime(
-                            DEFAULT_SERVER_DATETIME_FORMAT
-                        ),
-                    },
-                ),
-            ],
-            "disc_method": "percent",
-            "disc_amount": 500,
-        }
 
         self.order_obj = self.env["purchase.order"]
 
@@ -95,7 +56,7 @@ class TestPurchaseOrder(TransactionCase):
 
     def _prepare_order_with_percent_disc(self):
         data = self._prepare_base_vals()
-        data.update({"disc_method": "percent", "disc_amount": 500})
+        data.update({"disc_method": "percent", "disc_amount": 50})
 
         return data
 
